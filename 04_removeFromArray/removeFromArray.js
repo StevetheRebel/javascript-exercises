bbd5af780529bd190fce2e8d61d8277a770a6d24
StevetheRebel <124534650+StevetheRebel@users.noreply.github.com>
2023-04-20 17:07:14 +0300

    Tasks

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+  let result = arr.filter(item => !num.includes(item));
 
+  return result;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
