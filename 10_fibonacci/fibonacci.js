af4f30558e4d6502ba9a196098ef389e83b19dbb
StevetheRebel <124534650+StevetheRebel@users.noreply.github.com>
2023-07-13 17:11:31 +0300

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+	let fibArr = [];
+  
+	let n1 = 0,
+	  n2 = 1,
+	  nextTerm;
+  
+	for (let i = 1; i <= num + 1; i++) {
+	  fibArr.push(n1);
+	  nextTerm = n1 + n2;
+	  n1 = n2;
+	  n2 = nextTerm;
+	}
+  
+	return fibArr[num];
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function (arr) {
+	let titleArray = [];
+	let len = arr.length;
+  
+	for (let i = 0; i < len; i++) {
+	  titleArray.push(arr[i].title);
+	}
+  
+	return titleArray;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
