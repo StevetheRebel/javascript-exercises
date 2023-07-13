c1e162320652a745d750f209dc5b92df8e7bed47
StevetheRebel <124534650+StevetheRebel@users.noreply.github.com>
2023-07-13 19:26:00 +0300

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let ages = [];
+  let largestAge = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    let ageCalc = arr[i].yearOfDeath - arr[i].yearOfBirth;
+
+    ages.push(ageCalc);
+  }
+
+  for (let x of ages) {
+    if (x > largestAge) {
+      largestAge = x;
+    }
+  }
+
+  let ageIndex = ages.indexOf(largestAge);
+
+  return arr[ageIndex];
 };
 
 // Do not edit below this line
